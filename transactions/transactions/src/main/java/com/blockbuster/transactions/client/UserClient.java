e66d08d28b5fd1444049824a5876342422e68f0a
Sebastianidm <sebastiandiazmiranda@gmail.com>
2026-05-17 12:42:22 -0400

    FIX: se corrige feignClint de user al puerto 8082

--- transactions/transactions/src/main/java/com/blockbuster/transactions/client/UserClient.java
+++ transactions/transactions/src/main/java/com/blockbuster/transactions/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "ms-users", url = "http://localhost:8080/api/v1")
+@FeignClient(name = "ms-users", url = "http://localhost:8082/api/v1")
 public interface UserClient {
 
     @GetMapping("/users/{id}")
